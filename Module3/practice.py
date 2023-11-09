d3606634421abfce92b398796e9101a21e6efa04
ntnuglis <aaa1029a1029@gmail.com>
2023-11-09 15:55:43 +0800

    第一次上課修改

--- Module3/practice.py
+++ Module3/practice.py
@@ -1,8 +1,17 @@
 # 終極密碼 讓使用者猜數字
 
+num = 25
 
-
-
-
-
-
+while True:
+    user_input = int(input('請輸入數字 1 ~ 100 : '))
+    if user_input == num:
+        print('恭喜中獎')
+        break
+    elif user_input < 0 or user_input > 100:
+        print('請輸入 1 ~ 100 的數')
+    elif user_input < num:
+        print('請輸入更大的數')
+    elif user_input > num:
+        print('請輸入更小的數')
+    else:
+        print('輸入錯誤請重新輸入')
